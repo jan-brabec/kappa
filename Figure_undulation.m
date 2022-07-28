2e399969c42ac1abeba7e8b16b94b78fe01f56b2
Jan Brabec <45789777+jan-brabec@users.noreply.github.com>
2022-07-28 10:54:07 +0200

    Add files via upload

---
+++ Figure_undulation.m
@@ -0,0 +1,47 @@
+load('a.mat')
+
+
+c_1_harm = [14,167,181]./255;
+c_n_harm = [255,190,79]./255;
+c_stocha = [232,112,42]./255;
+
+clf;
+figure(10)
+plot([0 100],[0 100],'--','Color',pl_color('line'),'Linewidth',10,'HandleVisibility','off')
+hold on
+plot(f_delta_pred_1_harm,f_delta_est_1_harm,'.','Markersize',80,'Color',c_1_harm);
+plot(f_delta_pred_n_harm,f_delta_est_n_harm,'.','Markersize',80,'Color',c_n_harm);
+plot(f_delta_pred_stoch,f_delta_est_stoch,'.','Markersize',80,'Color',c_stocha);
+
+xlim([0 100])
+ylim([0 100])
+yticks([0 50 100])
+xticks([0 50 100])
+
+ylabel('Estimated {\itf}_{\Delta}')
+xlabel('Predicted {\itf}_{\Delta}')
+
+plot_set_1x3;
+legend({'1-harmonic','n-harmomic','stochastic'},'location','southeast','Fontsize',30)
+hold on
+
+figure(20)
+plot([0 1.1],[0 1.1],'--','Color',pl_color('line'),'Linewidth',10)
+hold on
+plot(D_hi_pred*1e9,D_hi_est*1e9,'.','Markersize',80,'Color',c_1_harm)
+plot(D_hi_pred_n_harm*1e9,D_hi_est_n_harm*1e9,'.','Markersize',80,'Color',c_n_harm)
+plot(D_hi_pred_stoch*1e9,D_hi_est_stoch*1e9,'.','Markersize',80,'Color',c_stocha)
+
+xlim([0 1.1])
+ylim([0 1.1])
+yticks([0 0.5 1])
+xticks([0 0.5 1])
+
+ylabel('Estimated {\itD}_{hi} [μm^2/ms]')
+xlabel('Predicted {\itD}_{hi} [μm^2/ms]')
+
+plot_set_1x3;
+legend off;
+
+
+
---
+++ restricted_between_planes_diffusion.m
@@ -0,0 +1,95 @@
+clear
+clf
+
+%Parameters
+r = 2e-5; %radius of plane restriction
+D0 = 2e-9;   %bulk diffusion coefficient
+p = 1e5;     %number of particles
+T = 500*1e-3;% max time
+no_T = 1001;
+
+%Init time
+t = linspace(0,T,no_T); %time
+dt = t(2) - t(1);
+n = round(T / dt);
+msd_x = zeros(no_T,1);
+msd_y = zeros(no_T,1);
+ADC_x = zeros(no_T,1);
+ADC_y = zeros(no_T,1);
+
+dx = sqrt(2*D0*dt); %stepping
+x = [unifrnd(r*10,+r*20,p,1), unifrnd(-r,+r,p,1)];
+ind = find(abs(x(:,2)) > r-r*0.05);
+x(ind,:) = [];
+plot(x(:,1),x(:,2),'.','Markersize',0.01)
+
+for ts = 1:n
+    x = x + randn(size(x,1),2) * dx; %Simulation step
+    
+    if ts == 1
+        x_init = x(:,1); %record initial position along x
+        y_init = x(:,2); %record initial position along y
+    end
+    
+    %apply restrictions
+    ind = find(x(:,2) > r);
+    x(ind,2) = r - (x(ind,2) - r);
+    ind = find(x(:,2) < -r);
+    x(ind,2) = -r + abs(x(ind,2))-r;
+    %
+    
+    msd_x(ts) = mean((x(:,1)-x_init).^2); %mean square displacement in x
+    msd_y(ts) = mean((x(:,2)-y_init).^2); %mean square displacement in y
+    ADC_x(ts) = 1/2 * msd_x(ts)/t(ts); %apparent diffusion coeffiecient in x
+    ADC_y(ts) = 1/2 * msd_y(ts)/t(ts); %apparent diffusion coeffiecient in y
+    
+    
+    if (0)
+        clf;
+        
+        x_col = [255,197,1]./255;
+        y_col = [188,14,76]./255;
+        p_col = [53,79,96]./255;
+        l_width = 2;
+        font_size = 20;
+        smooth_f = 5;
+        
+        subplot(1,3,1) %particles
+        scatter(x(:,1),x(:,2),1,p_col);
+%         axis equal
+        ylim([-0.3e-4 0.7e-4])
+        xlim([0.5e-4 5.5e-4])
+        axis off
+        
+        
+        subplot(1,3,2) %msd
+        hold on
+        plot(t(1:ts)*1e3,msd_x(1:ts)*1e12,'Color',x_col,'Linewidth',l_width)
+        plot(t(1:ts)*1e3,msd_y(1:ts)*1e12,'Color',y_col,'Linewidth',l_width)
+        ylim([0 2000])
+        xlim([0 T*1e3])
+        ax = gca; ax.FontSize = font_size;
+        xticks([0 250 500])
+        yticks([0 1000 2000])
+        ax.TickLength = [0.02 0.02];
+        set(ax,'tickdir','out'); set(ax,'linewidth',2); set(gca,'box','off')
+        axis square;   
+        
+        subplot(1,3,3) %ADC
+        hold on
+        plot(t(1:ts)*1e3,smooth(ADC_x(1:ts),smooth_f)*1e9,'Color',x_col,'Linewidth',l_width)
+        plot(t(1:ts)*1e3,smooth(ADC_y(1:ts),smooth_f)*1e9,'Color',y_col,'Linewidth',l_width)
+        ylim([0 3])
+        xlim([0 T*1e3])
+        ax = gca; ax.FontSize = font_size;
+        xticks([0 250 500])
+        yticks([0 1 2 3])
+        ax.TickLength = [0.02 0.02];
+        set(ax,'tickdir','out'); set(ax,'linewidth',2); set(gca,'box','off')
+        axis square;    
+        
+        drawnow;
+        hold off
+    end
+    
+end
